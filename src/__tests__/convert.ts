866401c1274663303b59a40239b480025e18a3b7
Rai Butera <rai@rbutera.com>
2022-06-10 16:34:00 +0100

    trim end and remove <cite> tags

--- src/__tests__/convert.ts
+++ src/__tests__/convert.ts
@@ -5,8 +5,13 @@ describe('convert', () => {
     const input = `I love [md2slack](https://github.com/rbutera/md2slack)`
     const actual = convert(input)
 
-    expect(actual).toBe(
-      'I love <https://github.com/rbutera/md2slack|md2slack>\n',
-    )
+    expect(actual).toBe('I love <https://github.com/rbutera/md2slack|md2slack>')
+  })
+
+  it('converts <cite> tags to italics', () => {
+    const input = `wawawiwa <cite>Borat</cite>`
+    const actual = convert(input)
+
+    expect(actual).toBe('wawawiwa _Borat_')
   })
 })
--- src/convert.ts
+++ src/convert.ts
@@ -3,7 +3,8 @@ import slackify from 'slackify-markdown'
  * Converts a markdown string to slack markdown
  */
 function convert(input: string): string {
-  return slackify(input)
+  const slackified = slackify(input)
+  return slackified.replace(/<(\/)?cite>/g, '_').trimEnd()
 }
 
 export default convert

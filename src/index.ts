fe0b3c8047254040571d10966aa80f6587d931c7
Rai Butera <rai@rbutera.com>
2022-08-10 15:40:32 +0100

    fix console again

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 #! /usr/bin/env node
 import convert from './convert'
 import * as process from 'node:process'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import * as packageJson from '../package.json'
 
-export function main() {
+export function run() {
   process.stdin.on('readable', () => {
     let chunk
     while ((chunk = process.stdin.read()) !== null) {
@@ -10,6 +13,20 @@ export function main() {
       process.stdout.write(converted)
     }
   })
+}
+
+export function version() {
+  process.stdout.write(packageJson.version)
+}
+
+export function main() {
+  const argv = yargs(hideBin(process.argv)).argv
+  if (Object.keys(argv).includes('v')) {
+    version()
+    return
+  }
+
+  run()
 }
 
 main()
